eb6c5f9ab18ecd62d1c83fae719808255d1670b0
AbdulalimLadan <ladanski_11>
2020-11-25 13:43:48 +0100

    [Ui design]

---
+++ app/src/main/java/com/z4rtx/weatheria/MainActivity.java
@@ -0,0 +1,14 @@
+package com.z4rtx.weatheria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
